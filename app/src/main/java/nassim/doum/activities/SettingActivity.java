9809b37c416bd958a8cc66c8a4edaa267fb7d84e
tread <ca_djenane@esi.dz>
2016-04-24 01:17:37 +0100

    this is a commit message

---
+++ app/src/main/java/nassim/doum/activities/SettingActivity.java
@@ -0,0 +1,18 @@
+package nassim.doum.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import nassim.doum.R;
+
+/**
+ * Created by tread on 24/04/2016.
+ */
+public class SettingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
+
